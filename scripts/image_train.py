fc929271098504f407edddc0a3480a32c9fbfdfe
DCZ <dennischenxi@gmail.com>
2024-04-15 15:08:33 +0200

    Commented out visdom imports and initialization

--- scripts/image_train.py
+++ scripts/image_train.py
@@ -17,8 +17,8 @@ from guided_diffusion.script_util import (
     add_dict_to_argparser,
 )
 from guided_diffusion.train_util import TrainLoop
-from visdom import Visdom
-viz = Visdom(port=8097)
+# from visdom import Visdom
+# viz = Visdom(port=8097)
 
 def main():
     args = create_argparser().parse_args()
