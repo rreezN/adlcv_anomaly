d62e570af1aa59ac5817222d3c522dc102b16f30
MarieJuhlJ <121543742+MarieJuhlJ@users.noreply.github.com>
2024-04-30 16:58:19 +0200

    Change to numpy

--- guided_diffusion/sampleloader.py
+++ guided_diffusion/sampleloader.py
@@ -7,7 +7,7 @@ import numpy as np
 class SampledDataset(Dataset):
     def __init__(self, root_dir):
         self.root_dir = root_dir
-        self.image_paths = [os.path.join(root_dir, filename) for filename in os.listdir(root_dir) if "output" in filename and filename.endswith(".png")]
+        self.image_paths = [os.path.join(root_dir, filename) for filename in os.listdir(root_dir) if "output" in filename and filename.endswith(".npy")]
 
     def __len__(self):
         return len(self.image_paths) // 4
@@ -16,7 +16,8 @@ class SampledDataset(Dataset):
         image_stack = []
         for i in range(4):
             image_path = self.image_paths[idx * 4 + i]
-            image = Image.open(image_path)
-            image = torch.tensor(np.array(image)).float() # / 255.0  # Normalize pixel values
-            image_stack.append(image)
+            image = np.load(image_path)
+            image_stack.append(torch.tensor(image).float())
+        stck = torch.stack(image_stack)
+        print(stck.shape)
         return (torch.stack(image_stack))
