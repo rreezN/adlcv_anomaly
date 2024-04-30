41b0137cd9e239a0322dc094717313b21ed98242
MarieJuhlJ <121543742+MarieJuhlJ@users.noreply.github.com>
2024-04-30 16:58:48 +0200

    Added forward for FID

--- guided_diffusion/unet.py
+++ guided_diffusion/unet.py
@@ -907,4 +907,30 @@ class EncoderUNetModel(nn.Module):
         else:
             h = h.type(x.dtype)
             return self.out(h)
+        
+    def forward_feature_space(self, x, timesteps):
+        """
+        Apply the model to an input batch removing the last out layer.
+
+        :param x: an [N x C x ...] Tensor of inputs.
+        :param timesteps: a 1-D batch of timesteps.
+        :return: an [N x K] Tensor of outputs.
+        """
+        emb = self.time_embed(timestep_embedding(timesteps, self.model_channels))
+
+        results = []
+        h = x.type(self.dtype)
+        for module in self.input_blocks:
+            h = module(h, emb)
+            if self.pool.startswith("spatial"):
+                results.append(h.type(x.dtype).mean(dim=(2, 3)))
+        h = self.middle_block(h, emb)
+    
+        if self.pool.startswith("spatial"):
+            results.append(h.type(x.dtype).mean(dim=(2, 3)))
+            h = th.cat(results, axis=-1)
+            return h
+        else:
+            h = h.type(x.dtype)
+            return h
 
